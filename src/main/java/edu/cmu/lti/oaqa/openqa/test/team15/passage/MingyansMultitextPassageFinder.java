ed77ea2cbe7b56abda44edace0b903c7afbbc687
mingyans <mingyans@andrew.cmu.edu>
2012-11-28 17:00:11 -0500

    REpair passage retrieval part and can run now!

--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team15/passage/MingyansMultitextPassageFinder.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team15/passage/MingyansMultitextPassageFinder.java
@@ -1,7 +1,6 @@
 package edu.cmu.lti.oaqa.openqa.test.team15.passage;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,89 +9,96 @@ import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 
 import edu.cmu.lti.oaqa.framework.data.PassageCandidate;
 import edu.cmu.lti.oaqa.openqa.hello.passage.KeytermWindowScorer;
+import edu.cmu.lti.oaqa.openqa.hello.passage.KeytermWindowScorerProduct;
+import edu.cmu.lti.oaqa.openqa.hello.passage.KeytermWindowScorerSum;
+import edu.cmu.lti.oaqa.openqa.hello.passage.PassageCandidateFinder;
 
 public class MingyansMultitextPassageFinder {
   private String text;
+
   private String docId;
-  
-  private int textSize;      // values for the entire text
-  private int totalMatches;  
+
+  private int textSize; // values for the entire text
+
+  private int totalMatches;
+
   private int totalKeyterms;
-  
+
   private KeytermWindowScorer scorer;
-  
-  public MingyansMultitextPassageFinder( String docId , String text  ) {
+
+  public MingyansMultitextPassageFinder(String docId, String text) {
     super();
     this.text = text;
     this.docId = docId;
     this.textSize = text.length();
   }
-  public List<PassageCandidate> extractPassages( String[] keyterms ) {
+
+  public List<PassageCandidate> extractPassages(String[] keyterms) {
     List<List<PassageSpan>> matchingSpans = new ArrayList<List<PassageSpan>>();
     List<PassageSpan> matchedSpans = new ArrayList<PassageSpan>();
-    
+
     // Find all keyterm matches.
-    for ( String keyterm : keyterms ) {
-      Pattern p = Pattern.compile( keyterm );
-      Matcher m = p.matcher( text );
-      
-      
-      
-      
-      while ( m.find() ) {
-        PassageSpan match = new PassageSpan( m.start() , m.end() ) ;
-        matchedSpans.add( match );
+    for (String keyterm : keyterms) {
+      Pattern p = Pattern.compile(keyterm);
+      Matcher m = p.matcher(text);
+      while (m.find()) {
+        PassageSpan match = new PassageSpan(m.start(), m.end());
+        matchedSpans.add(match);
         totalMatches++;
       }
-      if (! matchedSpans.isEmpty() ) {
-        matchingSpans.add( matchedSpans );
+      if (!matchedSpans.isEmpty()) {
+        matchingSpans.add(matchedSpans);
         totalKeyterms++;
       }
     }
-    
+
     // create set of left edges and right edges which define possible windows.
     List<Integer> leftEdges = new ArrayList<Integer>();
     List<Integer> rightEdges = new ArrayList<Integer>();
-    for ( List<PassageSpan> keytermMatches : matchingSpans ) {
-      for ( PassageSpan keytermMatch : keytermMatches ) {
+    for (List<PassageSpan> keytermMatches : matchingSpans) {
+      for (PassageSpan keytermMatch : keytermMatches) {
         Integer leftEdge = keytermMatch.begin;
-        Integer rightEdge = keytermMatch.end; 
-        if (! leftEdges.contains( leftEdge ))
-          leftEdges.add( leftEdge );
-        if (! rightEdges.contains( rightEdge ))
-          rightEdges.add( rightEdge );
+        Integer rightEdge = keytermMatch.end;
+        if (!leftEdges.contains(leftEdge))
+          leftEdges.add(leftEdge);
+        if (!rightEdges.contains(rightEdge))
+          rightEdges.add(rightEdge);
       }
     }
-    
-    // For every possible window, calculate keyterms found, matches found; score window, and create passage candidate.
+
+    // For every possible window, calculate keyterms found, matches found; score window, and create
+    // passage candidate.
     List<PassageCandidate> result = new ArrayList<PassageCandidate>();
-    for ( Integer begin : leftEdges ) {
-      for ( Integer end : rightEdges ) {
-        if ( end <= begin ) continue; 
+    for (Integer begin : leftEdges) {
+      for (Integer end : rightEdges) {
+        if (end <= begin)
+          continue;
         // This code runs for each window.
         int keytermsFound = 0;
         int matchesFound = 0;
-        for ( List<PassageSpan> keytermMatches : matchingSpans ) {
+        for (List<PassageSpan> keytermMatches : matchingSpans) {
           boolean thisKeytermFound = false;
-          for ( PassageSpan keytermMatch : keytermMatches ) {
-            if ( keytermMatch.containedIn( begin , end ) ){
+          for (PassageSpan keytermMatch : keytermMatches) {
+            if (keytermMatch.containedIn(begin, end)) {
               matchesFound++;
               thisKeytermFound = true;
             }
           }
-          if ( thisKeytermFound ) keytermsFound++;
+          if (thisKeytermFound)
+            keytermsFound++;
         }
-        double score = scorer.scoreWindow( begin , end , matchesFound , totalMatches , keytermsFound , totalKeyterms , textSize );
+        double score = scorer.scoreWindow(begin, end, matchesFound, totalMatches, keytermsFound,
+                totalKeyterms, textSize);
         PassageCandidate window = null;
         try {
-          window = new PassageCandidate( docId , begin , end , (float) score , null );
+          window = new PassageCandidate(docId, begin, end, (float) score, null);
         } catch (AnalysisEngineProcessException e) {
           e.printStackTrace();
         }
-        result.add( window );
+        result.add(window);
       }
     }
-    
+
     // Sort the result in order of decreasing score.
     // Collections.sort ( result , new PassageCandidateComparator() );
     return result;
@@ -101,29 +107,49 @@ public class MingyansMultitextPassageFinder {
 
   class PassageSpan {
     private int begin, end;
-    public PassageSpan( int begin , int end ) {
+
+    public PassageSpan(int begin, int end) {
       this.begin = begin;
       this.end = end;
     }
-    public boolean containedIn ( int begin , int end ) {
-      if ( begin <= this.begin && end >= this.end ) {
+
+    public boolean containedIn(int begin, int end) {
+      if (begin <= this.begin && end >= this.end) {
         return true;
       } else {
         return false;
       }
     }
   }
-  
-  public double scoreWindowSum ( int begin , int end , int matchesFound , int totalMatches , int keytermsFound , int totalKeyterms , int textSize ){
+
+  public double scoreWindowSum(int begin, int end, int matchesFound, int totalMatches,
+          int keytermsFound, int totalKeyterms, int textSize) {
     int windowSize = end - begin;
-    double offsetScore = ( (double)textSize - (double)begin ) / (double)textSize;
-    return ( .25d * (double)matchesFound / (double)totalMatches ) + .25d * ( (double)keytermsFound / (double)totalKeyterms) + .25d * ( 1 - ( (double)windowSize / (double)textSize ) + .25d * offsetScore );
-  }
-  
-  public double scoreWindowProdcut ( int begin , int end , int matchesFound , int totalMatches , int keytermsFound , int totalKeyterms , int textSize ){
-    int windowSize = end - begin;
-    double offsetScore = ( (double)textSize - (double)begin ) / (double)textSize;
-    return ( (double)matchesFound / (double)totalMatches ) * ( (double)keytermsFound / (double)totalKeyterms) * ( 1 - ( (double)windowSize / (double)textSize ) * offsetScore );
+    double offsetScore = ((double) textSize - (double) begin) / (double) textSize;
+    return (.25d * (double) matchesFound / (double) totalMatches) + .25d
+            * ((double) keytermsFound / (double) totalKeyterms) + .25d
+            * (1 - ((double) windowSize / (double) textSize) + .25d * offsetScore);
   }
 
+  public double scoreWindowProdcut(int begin, int end, int matchesFound, int totalMatches,
+          int keytermsFound, int totalKeyterms, int textSize) {
+    int windowSize = end - begin;
+    double offsetScore = ((double) textSize - (double) begin) / (double) textSize;
+    return ((double) matchesFound / (double) totalMatches)
+            * ((double) keytermsFound / (double) totalKeyterms)
+            * (1 - ((double) windowSize / (double) textSize) * offsetScore);
+  }
+
+  public static void main(String[] args) {
+    PassageCandidateFinder passageFinder1 = new PassageCandidateFinder("1",
+            "The quick brown fox jumped over the quick brown fox.",
+            new KeytermWindowScorerProduct());
+    PassageCandidateFinder passageFinder2 = new PassageCandidateFinder("1",
+            "The quick brown fox jumped over the quick brown fox.", new KeytermWindowScorerSum());
+    String[] keyterms = { "quick", "jumped" };
+    List<PassageCandidate> windows1 = passageFinder1.extractPassages(keyterms);
+    System.out.println("Windows (product scoring): " + windows1);
+    List<PassageCandidate> windows2 = passageFinder2.extractPassages(keyterms);
+    System.out.println("Windows (sum scoring): " + windows2);
+  }
 }
