e6dcb6f3ad33d47b92f4ffe74ded2ad2c4e3424b
bolei <bolei@ghc78.ghc.andrew.cmu.edu>
2012-11-28 01:21:51 -0500

    begin to implement TF-IDF scoring passage retrieval mechanism

---
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team15/passage/candidate/BoLeiTfIdfCandidateFinder.java
@@ -0,0 +1,12 @@
+package edu.cmu.lti.oaqa.openqa.test.team15.passage.candidate;
+
+import java.util.List;
+
+import edu.cmu.lti.oaqa.framework.data.PassageCandidate;
+
+public class BoLeiTfIdfCandidateFinder {
+  public List<PassageCandidate> extractPassages(List<String> keyterms) {
+    // TODO
+    return null;
+  }
+}
