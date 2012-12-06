b01f128859649070163d93224a242fa81e35ecb6
bolei <bolei@GHC90.GHC.ANDREW.CMU.EDU>
2012-12-06 11:53:59 -0500

    removed unused import package

--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team15/keyterm/expand/MingyansSynonymKeytermExpander.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team15/keyterm/expand/MingyansSynonymKeytermExpander.java
@@ -12,7 +12,6 @@ import java.util.List;
 import org.apache.uima.UimaContext;
 
 import com.aliasi.tokenizer.PorterStemmerTokenizerFactory;
-import com.aliasi.tokenizer.TokenizerFactory;
 
 public class MingyansSynonymKeytermExpander extends KeytermExpander {
 
